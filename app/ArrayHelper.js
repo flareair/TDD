2f3e3c7f4630308b6e9edcb6af6862e6d8f6cd80
flareair <flareair0@gmail.com>
2016-09-01 11:38:46 +0300

    Add comments

--- app/ArrayHelper.js
+++ app/ArrayHelper.js
@@ -1,7 +1,15 @@
+/**
+ * Array helper class, includes several useful helpers
+ */
+
 let ArrayHelper = (function() {
 
     function ArrayHelper() {
-
+        /**
+         * Calculate sum of array items
+         * @param  {array} arr input array
+         * @return {number} result of operation
+         */
         this.sum = function sum(arr) {
 
             if (!Array.isArray(arr)) {
