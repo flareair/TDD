59ba42dbf526e8993261300bb10830fac5a0278e
flareair <flareair0@gmail.com>
2016-09-06 13:18:37 +0300

    Add sinon and first smoke test

--- qunit_tests/ArrayHelper.qunit.js
+++ qunit_tests/ArrayHelper.qunit.js
@@ -1,1 +1,24 @@
-console.log('test!');
+/*global ArrayHelper*/
+/*global sinon*/
+/*global equal*/
+
+(function() {
+    var arrayHelper;
+
+    module('ArrayHelper module', {
+        setup: function () {
+            sinon.sandbox.create();
+            arrayHelper = new ArrayHelper();
+        },
+
+        teardown: function () {
+            sinon.sandbox.restore();
+        }
+    });
+
+
+    test('Smoke test', () => {
+        equal(1, 1, 'One equal one');
+    });
+
+})();
